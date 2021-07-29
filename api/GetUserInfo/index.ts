b801df749c5710f7867d225197239dd5183cb048
CharaLagou <lagou.chara@gmail.com>
2021-07-29 10:06:55 +0200

    Update GetUserInfo API Function

--- api/GetUserInfo/index.ts
+++ api/GetUserInfo/index.ts
@@ -1,6 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { CosmosClient, Database, Container } from "@azure/cosmos";
 
+interface IUser {
+  id: "string";
+  groupID: "string";
+  username: "string";
+}
+
 const endpoint = "https://ng-girls-hackalearn-db-chara.documents.azure.com:443/";
 const key = process.env.DB_KEY;
 const db_name = "ng-girls-hackalearn-db-chara";
@@ -15,6 +21,10 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
     const query = `SELECT * FROM c WHERE c.groupID = "2"`;
     const groupRes = await container.items.query(query).fetchAll();
+
+    // Fetch data without query
+    let itemRes = await container.item("1", "2").read<IUser>();
+
     if (groupRes.resources.length === 0) {
       context.res = {
         status: 404,
@@ -27,12 +37,19 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
       return;
     }
 
+    // context.res = {
+    //     // status: 200, /* Defaults to 200 */
+    //     body: {
+    //       groupRes
+    //     }
+    // };
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: {
-          groupRes
-        }
-    };
+      // status: 200, /* Defaults to 200 */
+      body: {
+        groupRes,
+        item: itemRes.resource
+      }
+  };
 
 };
 
